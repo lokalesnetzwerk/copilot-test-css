05e9419e1a77ed7973812d963a9ea58880a6320a
Max <max.joel.mj@googlemail.com>
2024-12-10 14:08:17 +0100

    first test and new days between endpoint

--- my-quarkus-app/src/main/java/com/example/GreetingResource.java
+++ my-quarkus-app/src/main/java/com/example/GreetingResource.java
@@ -3,7 +3,12 @@ package com.example;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 @Path("/hello")
 public class GreetingResource {
@@ -12,5 +17,18 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from Quarkus REST";
+    }
+
+    @GET
+    @Path("/daysBetween")
+    @Produces(MediaType.TEXT_HTML)
+    public String daysBetween(@QueryParam("startDate") String startDate, @QueryParam("endDate") String endDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate start = LocalDate.parse(startDate, formatter);
+        LocalDate end = LocalDate.parse(endDate, formatter);
+        long daysBetween = ChronoUnit.DAYS.between(start, end);
+
+        return "<div style='text-align: center; font-size: 64px; color: rainbow;'>"
+                + daysBetween + " days</div>";
     }
 }
--- my-quarkus-app/src/test/java/com/example/GreetingResourceTest.java
+++ my-quarkus-app/src/test/java/com/example/GreetingResourceTest.java
@@ -7,9 +7,10 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-class GreetingResourceTest {
+public class GreetingResourceTest {
+
     @Test
-    void testHelloEndpoint() {
+    public void testHelloEndpoint() {
         given()
           .when().get("/hello")
           .then()
@@ -17,4 +18,12 @@ class GreetingResourceTest {
              .body(is("Hello from Quarkus REST"));
     }
 
+    @Test
+    public void testDaysBetweenEndpoint() {
+        given()
+          .when().get("/hello/daysBetween?startDate=2023-01-01&endDate=2023-01-10")
+          .then()
+             .statusCode(200)
+             .body(is("<div style='text-align: center; font-size: 64px; color: rainbow;'>9 days</div>"));
+    }
 }
